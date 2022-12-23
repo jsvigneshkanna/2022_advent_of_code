2274cb69fa0ed743bedab65b7c1e9d4028a6f3a8
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-23 15:59:04 +0530

    day5 initial commit

---
+++ day5/main.py
@@ -0,0 +1,10 @@
+# link - https://adventofcode.com/2022/day/5
+
+
+def calculate_part1():
+    with open('./input.txt', 'r') as data_file:
+        input = data_file.readlines()
+        pass
+
+if __name__ == '__main__':
+    calculate_part1()
