d3f89672a5ef98ebf172becadbb15edb9afca3ed
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-23 16:36:02 +0530

    day6 completed

--- day6/main.py
+++ day6/main.py
@@ -1,83 +1,28 @@
-# link - https://adventofcode.com/2022/day/5
+# link - https://adventofcode.com/2022/day/6
+
+def calculate_part1():
+    with open('./input.txt', 'r') as data_file:
+        input_lines = data_file.readlines()[0]
+        marker_loc = 4
+        for index in range(4, len(input_lines)):
+            set_len = set(input_lines[index-4: index])
+            if len(set_len) == 4:
+                marker_loc=  index
+                break
+        print('part1 answer: ', marker_loc)
+
+def calculate_part2():
+    with open('./input.txt', 'r') as data_file:
+        input_lines = data_file.readlines()[0]
+        marker_loc = 14
+        for index in range(14, len(input_lines)):
+            set_len = set(input_lines[index-14: index])
+            if len(set_len) == 14:
+                marker_loc=  index
+                break
+        print('part2 answer: ', marker_loc)
 
 
-from string import ascii_uppercase
-
-# Getting input
-with open('./input.txt') as file:
-    stack_strings, instructions = (i.splitlines() for i in file.read().strip('\n').split('\n\n'))
-
-"""
-'stacks' is a dictionary that will store {stack number}:{[characters in stack]}
-'indexes' is a list that stores the indexes in which the characters needed to fill 
-    the stacks will be found. 
-"""
-stacks = {int(digit):[] for digit in stack_strings[-1].replace(" ","")}
-indexes = [index for index, char in enumerate(stack_strings[-1]) if char != " "]
-
-# Used to display the contents in each stack
-def displayStacks():
-    print("\n\nStacks:\n")
-    for stack in stacks:
-        print(stack, stacks[stack])
-    print("\n")
-
-
-# Loading the stacks off the input
-def loadStacks():
-    for string in stack_strings[:-1]:
-        stack_num = 1
-        for index in indexes:
-            if string[index] == " ":
-                pass
-            else:
-                stacks[stack_num].insert(0, string[index])
-            stack_num += 1
-
-# Clearing the lists in every stack 
-def emptyStacks():
-    for stack_num in stacks:
-        stacks[stack_num].clear()
-
-# Used to get the characters at the end of every stack 
-def getStackEnds():
-    answer = ""
-    for stack in stacks:
-        answer += stacks[stack][-1]
-    return answer
-
-# === PART 1 ===
-loadStacks()
-for instruction in instructions:
-    instruction = instruction.replace("move", "").replace("from ", "").replace("to ", "").strip().split(" ")
-    instruction = [int(i) for i in instruction]
-
-    crates = instruction[0]
-    from_stack = instruction[1]
-    to_stack = instruction[2]
-
-    for crate in range(crates):
-        crate_removed = stacks[from_stack].pop()
-        stacks[to_stack].append(crate_removed)
-
-print("Answer to part 1: ", getStackEnds())
-
-
-# === PART 2 ===
-emptyStacks()
-loadStacks()
-for instruction in instructions:
-    instruction = instruction.replace("move", "").replace("from ", "").replace("to ", "").strip().split(" ")
-    instruction = [int(i) for i in instruction]
-
-    crates = instruction[0]
-    from_stack = instruction[1]
-    to_stack = instruction[2]
-
-    crates_to_remove = stacks[from_stack][-crates:]
-    stacks[from_stack] = stacks[from_stack][:-crates]
-
-    for crate in crates_to_remove:
-        stacks[to_stack].append(crate)
-
-print("Answer to part 2: ", getStackEnds())
+if __name__ == '__main__':
+    calculate_part1()
+    calculate_part2()
